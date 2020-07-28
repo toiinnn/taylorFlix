c68e7bb72bbb6dd0a2e812cd0021eec7efab360d
Antonio Marcos <marcos_antonio97@live.com>
2020-07-28 19:13:39 -0300

    Adição de roteamento e novas páginas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
+import '../../pages/cadastro/Video'
 import Button from '../Button';
 function Menu(){
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Toinflix logo" /> 
-            </a>
+            </Link>
 
-            <Button as="a" className="Button" href="/">    
+            <Button as={Link} className="Button" to="/cadastro/video">    
                 Novo vídeo
             </Button>
 
